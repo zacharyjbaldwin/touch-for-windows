834d000deff283e4ad9d2bda55559295b6d58977
zacharyjbaldwin <zacharyjbaldwin@gmail.com>
2022-08-31 15:05:45 -0500

    Added support for generating multiple files with successive numbering and for providing a date manually

--- bin/cli.js
+++ bin/cli.js
@@ -14,35 +14,72 @@ const argv = require('yargs/yargs')(process.argv.slice(2))
     .boolean('h').alias('h', 'hidden')
     .boolean('m')
 
+    .string('d').alias('d', 'date')
+
     .describe('a', 'change only the access time')
+    .describe('d', 'specifies the date to use instead of the current time')
     .describe('h', 'toggles hidden attribute for a file. creates a hidden file if it does not exist')
     .describe('m', 'change only the modification time')
     .describe('help', 'display this help and exit')
     .describe('version', 'output version information and exit')
+
+    .epilog('Note: The --date option accepts any value also accepted by the JavaScript Date object.')
     .argv;
 
-const FILES = argv._;
-for (let FILENAME of FILES) {
-    if (!FILENAME) {
-        console.log('No filename specified. Use touch --help for usage.');
+const rawFilesInput = argv._;
+let files = [];
+
+if (rawFilesInput.length == 0) {
+    console.log('No filename(s) specified');
+    process.exit(1);
+}
+
+const re = /(^.*){(\d+)\.\.\.(\d+)}(.*)$/;
+for (let file of rawFilesInput) {
+    if (file.match(re) != null) {
+        const parsed = file.match(re);
+        for (let i = +parsed[2]; i <= +parsed[3]; i++) {
+            files.push(newFile = parsed[1] + i + parsed[4]);
+        }
+    } else {
+        files.push(file);
+    }
+}
+
+let datetime = new Date();
+if (argv.d) {
+    datetime = new Date(argv.d);
+    if (!(datetime instanceof Date) || isNaN(datetime.valueOf())) {
+        console.log('Invalid date');
         process.exit(1);
     }
-    if (!fs.existsSync(FILENAME)) {
-        fs.writeFileSync(FILENAME, '')
+}
+
+for (let file of files) {
+    if (file.includes('\\') || file.includes('/') || file.includes(':') || file.includes('*') || file.includes('?') || file.includes('"') || file.includes('<') || file.includes('>') || file.includes('|')) {
+        console.log(`Invalid file name: ${file}`);
+        continue;
     }
-    if (argv.a && argv.m || (!argv.a && !argv.m && !argv.h)) {
-        const now = new Date();
-        fs.utimesSync(FILENAME, now, now);
+    
+    if (!fs.existsSync(file)) {
+        fs.writeFileSync(file, '')
     }
+
+    if (argv.a && argv.m || (!argv.a && !argv.m)) {
+        fs.utimesSync(file, datetime, datetime);
+    }
+
     if (argv.a && !argv.m) {
-        fs.utimesSync(FILENAME, new Date(), fs.statSync(FILENAME).mtime);
+        fs.utimesSync(file, datetime, fs.statSync(file).mtime);
     }
+
     if (!argv.a && argv.m) {
-        fs.utimesSync(FILENAME, fs.statSync(FILENAME).atime, new Date());
+        fs.utimesSync(file, fs.statSync(file).atime, datetime);
     }
+
     if (argv.h) {
-        fswin.getAttributes(FILENAME, (attr) => {
-            fswin.setAttributesSync(FILENAME, { IS_HIDDEN: !attr.IS_HIDDEN });
+        fswin.getAttributes(file, (attr) => {
+            fswin.setAttributesSync(file, { IS_HIDDEN: !attr.IS_HIDDEN });
         });
     }
 }
--- readme.md
+++ readme.md
@@ -20,10 +20,21 @@ For developers:
 | option | functionality |
 | --- | --- |
 | `-a` | change only the access time of a file |
+| `-d`, `--date` | specifies the date to use when changing access and/or modification/write time (accepts any date string that can be used by the JavaScript Date object
 | `-h`, `--hidden` | hide/unhide a file or create a hidden file if it doesn't exist
 | `-m` | change only the modification/write time of a file |
 | `-v`, `--version` | output version information and exit | 
 | `--help` | display help page |
+
+## Special Formatting
+
+For creating mutliple files with successive numbering, you can use a shortcut like this:
+
+`touch my-file{0...3}.txt`
+
+This command will create `my-file0.txt`, `my-file1.txt`, `my-file2.txt`, and `my-file3.txt`.
+
+All files will have the same attributes based on the flags provided to the command.
 
 ## Examples
 
@@ -38,6 +49,12 @@ Change both access time and modification/write time: `touch -am my-file.txt` (th
 Toggle hidden attribute of a file: `touch -h my-file.txt` (hides/unhides a file if it exists or creates a hidden file if it does not)
 
 Touch multiple files: `touch my-file.txt package.json another-file.js`
+
+Manually specify the date to use: `touch -d="8/25/2054 6:35:56 AM" my-file.txt`
+
+Using multiple flags together: `touch -amh -d="5/4/2020" my-file.txt`
+
+Creating multiple files: `touch my-file{0...5}.txt`
 
 ## License
 
